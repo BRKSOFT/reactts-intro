d62bac07facd82edee4ca967b7cc41df6f9ba2af
neominal.solutions <neominal.solutions@gmail.com>
2024-07-12 11:32:45 +0300

    son hali

--- src/components/class.component.fetch.sample.tsx
+++ src/components/class.component.fetch.sample.tsx
@@ -31,23 +31,22 @@ export class ClassComponentFetchSample extends Component<Props, State> {
 	componentDidMount(): void {
 		console.log('...init state-01');
 		this.loadDataAsync();
-
-		// fetch('https://jsonplaceholder.typicode.com/todos')
-		// 	.then((response) => {
-		// 		console.log('response', response);
-		// 		return response.json(); // fetch kullanırsak response stream olarak gelir ama bize json tipi lazım o yüzden response streamden json parse ediyoruz
-		// 	})
-		// 	.then((data) => {
-		// 		// json dönmüş data var
-		// 		console.log('data', data);
-		// 		this.setState({ ...this.state, todos: data });
-		// 	})
-		// 	.catch((err) => {
-		// 		console.log('err', err);
-		// 	});
-
-		// Not lifecycle methodlar içersinde async await gibi kodlar yazamasak da async bir method çağırabiliriz.
 	}
+	// fetch('https://jsonplaceholder.typicode.com/todos')
+	// 	.then((response) => {
+	// 		console.log('response', response);
+	// 		return response.json(); // fetch kullanırsak response stream olarak gelir ama bize json tipi lazım o yüzden response streamden json parse ediyoruz
+	// 	})
+	// 	.then((data) => {
+	// 		// json dönmüş data var
+	// 		console.log('data', data);
+	// 		this.setState({ ...this.state, todos: data });
+	// 	})
+	// 	.catch((err) => {
+	// 		console.log('err', err);
+	// 	});
+
+	// Not lifecycle methodlar içersinde async await gibi kodlar yazamasak da async bir method çağırabiliriz.
 
 	render(): ReactNode {
 		console.log('...rendering-01');
